b0647e55c2e324b8d324be1142ba8bc54b247f48
SamusVD <samueldanso@outlook.fr>
2020-02-25 14:58:59 +0100

    Initialize repo

---
+++ src/main/kotlin/com/personal/halloffame/HalloffameApplication.kt
@@ -0,0 +1,11 @@
+package com.personal.halloffame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HalloffameApplication
+
+fun main(args: Array<String>) {
+	runApplication<HalloffameApplication>(*args)
+}
---
+++ src/test/kotlin/com/personal/halloffame/HalloffameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.personal.halloffame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HalloffameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
